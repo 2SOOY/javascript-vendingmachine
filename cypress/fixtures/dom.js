9c7d825e55bf7d41fdc4327ae81c922249065bf4
2sooy <1401660@naver.com>
2021-08-04 12:11:01 +0900

    fix: cypress dom 상수 초기화

---
+++ cypress/fixtures/dom.js
@@ -0,0 +1,49 @@
+export const MENU = {
+  PURCHASE: "#product-purchase-menu",
+  PRODUCT: "#product-manage-menu",
+  VENDING: "#vending-machine-manage-menu",
+};
+
+export const CHARGE = {
+  INPUT: "#charge-input",
+  BUTTON: "#charge-button",
+  SPAN: "#charge-amount",
+};
+
+export const PURCHASE = {
+  PRODUCT_NAME: ".product-purchase-name",
+  PRODUCT_PRICE: ".product-purchase-price",
+  PRODUCT_QUANTITY: ".product-purchase-quantity",
+  PRODUCT_BUTTON: ".purchase-button",
+
+  PRODUCT_LIST: ".purchased-item",
+};
+
+export const CHANGE = {
+  BUTTON: "#coin-return-button",
+  COIN_500: "#coin-500-quantity",
+  COIN_100: "#coin-100-quantity",
+  COIN_50: "#coin-50-quantity",
+  COIN_10: "#coin-10-quantity",
+};
+
+export const VENDING = {
+  CHARGE_INPUT: "#vending-machine-charge-input",
+  CHARGE_BUTTON: "#vending-machine-charge-button",
+
+  COIN_500: "vending-machine-coin-500-quantity",
+  COIN_100: "vending-machine-coin-100-quantity",
+  COIN_50: "vending-machine-coin-50-quantity",
+  COIN_10: "vending-machine-coin-10-quantity",
+};
+
+export const MANAGE = {
+  ADD_NAME: "#product-name-input",
+  ADD_PRICE: "#product-price-input",
+  ADD_QUANTITY: "#product-quantity-input",
+  ADD_BUTTON: "#product-add-button",
+
+  PRODUCT_NAME: ".product-manage-name",
+  PRODUCT_PRICE: ".product-manage-price",
+  PRODUCT_QUANTITY: ".product-manage-quantity",
+};
