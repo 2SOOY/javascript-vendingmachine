a468bc0494f340891dc96c1ccb8e9ca1ef413e5c
2sooy <1401660@naver.com>
2021-08-09 11:38:11 +0900

    feat: 상품 관리를 위한 productManager 클래스 추가

---
+++ src/js/productManager.js
@@ -0,0 +1,32 @@
+import { products } from "./lib/mock.js";
+
+class ProductManager {
+  constructor() {
+    this.products = products;
+  }
+
+  addProduct(product) {
+    const products = this.products.map((p) => (p.name === product.name ? product : p));
+
+    this.products = products;
+  }
+
+  purchaseProduct(name, quantity) {
+    const product = this.products.find((product) => product.name === name);
+
+    if (!product) {
+      throw new Error("존재하지 않는 상품을 구매하려고 시도했습니다.");
+    }
+
+    if (product.quantity < quantity) {
+      throw new Error("상품 수량이 부족합니다 상품을 추가해주세요");
+    }
+
+    product.quantity -= quantity;
+
+    const products = this.products.map((p) => (p.name === product.name ? product : p));
+    this.products = products;
+  }
+}
+
+export default ProductManager;
